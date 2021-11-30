d780e4ad32d73c8425537409e1fd78c958a90b11
khalifan-kfan <khalifanmuwonge@gmail.com>
2021-12-01 02:42:23 +0300

    abit of direction to upload a zip file

--- src/App.jsx
+++ src/App.jsx
@@ -6,67 +6,74 @@ import Input from './components/Input'
 import './App.css';
 
 const App = () => {
-  const [files, setFiles] = useState([]);
-  const [framework, setFramework] = useState('');
-  const [image, setImage] = useState({
-    name: '',
-    version: ''
-  });
+const [files, setFiles] = useState([]);
+const [framework, setFramework] = useState('');
+const [image, setImage] = useState({
+name: '',
+version: ''
+});
 
-  const getPathName = (path) => {
-    let filePath = path.replaceAll("/", "|").replace("|", "");
-    filePath = filePath.substring(filePath.indexOf("|")).replace("|", "");
-    return filePath;
-  }
+const getPathName = (path) => {
+let filePath = path.replaceAll("/", "|").replace("|", "");
+filePath = filePath.substring(filePath.indexOf("|")).replace("|", "");
+return filePath;
+}
 
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
+const handleChange = ({ target }) => {
+const { name, value } = target;
 
-    setImage({
-      ...image,
-      [name]: value
-    });
-  };
+setImage({
+...image,
+[name]: value
+});
+};
 
-  const handleDropdownChange = (selected) => {
+const handleDropdownChange = (selected) => {
 
-    setFramework(selected);
-  }
+setFramework(selected);
+}
 
-  const handleSubmit = () => {
-    const formData = new FormData();
-    const { name, version } = image;
+const handleSubmit = () => {
+const formData = new FormData();
+const { name, version } = image;
 
-    for (const file of files) {
-      const { path } = file;
-      console.log(getPathName(path));
-      formData.append("files", file, getPathName(path));
-    }
+for (const file of files) {
+const { path } = file;
+console.log(getPathName(path));
+formData.append("files", file, getPathName(path));
+}
 
-    formData.append("name", name);
-    formData.append("tag", version);
-    formData.append("framework", framework);
+formData.append("name", name);
+formData.append("tag", version);
+formData.append("framework", framework);
 
-    axios.post('http://localhost:5000/', formData)
-    .then()
-    .catch();
-  }
+axios.post('http://localhost:5000/', formData)
+.then()
+.catch();
+}
 
-  return (
-    <div className="App">
-      <div className="FrameWorkInput">
-        <Dropdown required name="framework" placeholder="Framework" options={['Html-CSS-JS', 'React', 'NodeJS']} onChange={handleDropdownChange} />
-      </div>
-      <div className="Inputs">
-        <Input name="name" placeholder="Image name" onChange={handleChange} value={image.name} required />
-        <Input name="version" placeholder="Version" onChange={handleChange} value={image.version} />
-      </div>
-      <div className="Dropzone">
-        <Dropzone handleDrop={(files) => setFiles(files)} />
-      </div>
-      <button onClick={handleSubmit}>DEPLOY</button>
-    </div>
-  );
+return (
+<div className="App">
+<div className="FrameWorkInput">
+<Dropdown required name="framework" placeholder="Framework" options={['Html-CSS-JS', 'React', 'NodeJS']} onChange={handleDropdownChange} />
+</div>
+<div className="Inputs">
+<Input name="name" placeholder="Image name" onChange={handleChange} value={image.name} required />
+<Input name="version" placeholder="Version" onChange={handleChange} value={image.version} />
+</div>
+<div>
+Please zip your project folder and upload only the zip file
+</div>
+<div>
+Avoid including build files in your zip file eg node_modules 
+</div>
+<div className="Dropzone">
+<Dropzone handleDrop={(files) => setFiles(files)} />
+</div>
+<button onClick={handleSubmit}>DEPLOY</button>
+</div>
+);
 };
 
 export default App;
+
